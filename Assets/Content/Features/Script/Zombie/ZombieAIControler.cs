4bdf9b5f7532302937c7179d7123b7d440740485
maximkyshka <tahanproyow@gmail.com>
2025-07-06 18:29:57 +0300

    add AI for zombie

--- Assets/Content/Features/Script/Zombie/ZombieAIControler.cs
+++ Assets/Content/Features/Script/Zombie/ZombieAIControler.cs
@@ -4,8 +4,9 @@ using UnityEngine.AI;
 public class ZombieAIControler : MonoBehaviour
 {
     private NavMeshAgent _agent;
-    [SerializeField] private Transform[] _targetPoint;
+    private Transform[] _targetPoint;
     private int _targetPointIndex;
+    private ZombieSpavnerControler _spavnerControler;
     
     void Start()
     {
@@ -14,11 +15,26 @@ public class ZombieAIControler : MonoBehaviour
     
     void Update()
     {
-        _agent.SetDestination(_targetPoint[_targetPointIndex].position);
-        
-        if (Vector3.Distance(transform.position, _targetPoint[_targetPointIndex].position) < 0.5f)
+        if (_targetPoint != null)
         {
-            _targetPointIndex++;
+            _agent.SetDestination(_targetPoint[_targetPointIndex].position);
+        
+            if (Vector3.Distance(transform.position, _targetPoint[_targetPointIndex].position) < 0.5f)
+            {
+                _targetPointIndex++;
+            }
         }
+    }
+    
+    public void Setup(Transform[] targetPoint, ZombieSpavnerControler spavnerControler)
+    {
+        _targetPoint = targetPoint;
+        
+        _spavnerControler = spavnerControler;
+    }
+    
+    public int GetCurentPointIndex()
+    {
+        return _targetPointIndex;
     }
 }
