901c7dea88ec39efbb4a6a793962e2124a6e7d13
maximkyshka <tahanproyow@gmail.com>
2025-07-12 12:17:01 +0300

    edit the script folder

--- Assets/Content/Features/Script/UI/ClickSoundController.cs
+++ Assets/Content/Features/Script/UI/ClickSoundController.cs
@@ -1,16 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickSoundController : MonoBehaviour
 {
     [SerializeField] private AudioClip _sound;
+    
+    [SerializeField] private bool _playSoundOnClick  = false;
 
     private AudioSource _audioSource;
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (_playSoundOnClick)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ClickSound();
+            }
+        }
     }
 
     public void ClickSound()
